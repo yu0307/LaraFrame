121055ef1b4dbcf1a8b5e529a24d132ae9958600
lucas <lucas.f.lu@nasa.gov>
2019-10-17 15:48:25 -0700

    PS

--- src/assets/js/controlpanel.js
+++ src/assets/js/controlpanel.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#control_CRUD').on('hidden.bs.modal', function (e) {
         $('#control_CRUD').find('.LF_CRUD').removeClass('show');
-        $('#control_CRUD input, #control_CRUD textarea, #control_CRUD select').val('').removeClass('checked').trigger('change');
+        $('#control_CRUD input:not([type="radio"],[type="checkbox"]), #control_CRUD textarea, #control_CRUD select').val('').removeClass('checked').trigger('change');
     })
     $('button.outlet_CRUD').on('click', function () {
         if ($(this).attr('outlet-target').length > 0) {
