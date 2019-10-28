d908f2f112eb94484f5cc3208b514068e5478e4e
lucas <lucas.f.lu@nasa.gov>
2019-10-28 15:03:52 -0700

    PS

--- src/http/controllers/fe_profileController.php
+++ src/http/controllers/fe_profileController.php
@@ -2,17 +2,14 @@
 
 namespace feiron\felaraframe\http\controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class fe_profileController extends Controller
 {
-    public function show(Request $request, $uid=null){
+    public function show(Request $request, $uid = null)
+    {
         $userProvider = config('auth.providers.' . config('auth.guards.web.provider') . '.model');
-        return view('felaraframe::profile',['User'=> (empty($uid)? auth()->user(): $userProvider::find($uid)), 'Editable'=> empty($uid)]);
-    }
-
-    public function UploadProfImg(Request $request){
-        
+        return view('felaraframe::profile', ['User' => (empty($uid) ? auth()->user() : $userProvider::find($uid)), 'Editable' => empty($uid)]);
     }
 }
