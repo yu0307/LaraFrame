97f722e5d6aa3f7c8a83ba189f434fade2f43632
lucas <lucas.f.lu@nasa.gov>
2019-10-31 11:19:50 -0700

    Finished control panel and profile page
    implemented control panel and user profile outlet

--- src/assets/js/controlpanel.js
+++ src/assets/js/controlpanel.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('#control_CRUD').on('hidden.bs.modal', function (e) {
-        $('#control_CRUD').find('.LF_CRUD').removeClass('show');
-        $('#control_CRUD input:not([type="radio"],[type="checkbox"]), #control_CRUD textarea, #control_CRUD select').val('').removeClass('checked').trigger('change');
+        clearWorkingArea();
     })
     $('button.outlet_CRUD').on('click', function () {
         if ($(this).attr('outlet-target').length > 0) {
@@ -15,6 +14,12 @@ function hideCRUD(callback) {
     if (typeof (callback) == 'function') {
         callback();
     }
+}
+
+function clearWorkingArea() {
+    $('#control_CRUD').find('.LF_CRUD').removeClass('show');
+    $('#control_CRUD input:not([type="radio"],[type="checkbox"]), #control_CRUD textarea, #control_CRUD select').val('').removeClass('checked').trigger('change');
+    $('#control_CRUD input[type="radio"],#control_CRUD input[type="checkbox"]').removeAttr('checked').trigger('change').iCheck('update');
 }
 
 function showCRUD(tar, preload = false) {
