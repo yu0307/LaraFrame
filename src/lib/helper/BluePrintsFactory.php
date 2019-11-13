2ee247bbdac7d4409d0171c2d673f7c760f00eda
Lucas <yu0307@gmail.com>
2019-11-12 21:57:37 -0800

    Migration

--- src/lib/helper/BluePrintsFactory.php
+++ src/lib/helper/BluePrintsFactory.php
@@ -20,9 +20,18 @@ class BluePrintsFactory {
             'withTimeStamps' => true,
             'unsigned' => false,
             'visible' => true,
-            'editable' => true
+            'editable' => true,
+            'engine'=>'InnoDB',
+            'charset_table'=>'utf8',
+            'charset_field'=>'utf8',
+            'collation'=>'utf8_unicode_ci'
         ]
     ];
+    private const FieldsWithSize=[
+        'char',
+        'string'
+    ];
+    private const migrationPath="database/migrations/";
 
     public function __construct($target,$storage)
     {
@@ -39,7 +48,7 @@ class BluePrintsFactory {
     public function buildModels(){
         $modelFiles= preg_grep('/^.*\.(mbp)$/i',$this->BlueprintStorage->files($this->projectPath.'/models'));
         if(empty($modelFiles)){
-            throw new Exception("There are no model files in the sub direcotry [models]");
+            echo ("There are no model files in the sub direcotry [models]\n");
         }else{
             foreach($modelFiles as $model){
                 $m =json_decode($this->BlueprintStorage->get($model));
@@ -48,10 +57,69 @@ class BluePrintsFactory {
         }
     }
 
-    private function ProcessModels($model){
+    private function ProcessModels($models){
         $path = 'app/model/';
-        $migrationPath = 'database/migrations/';
         
+        foreach($models as $model){
+            if(isset($model->modelName)){
+                $this->createMigration($model);
+            }
+        }
+    }
+
+    private function createMigration($model){
+        $className='create_'.$model->modelName.'_table';
+        $target=self::migrationPath.'fe_blueprint_migration_file_'.$className.'.php';
+        if($this->RootStorage->exists($target)===false){
+            $fieldList="";
+            foreach($model->modelFields as $field){
+                $fieldList.='
+                $table->'.($field->dataType??self::Defaults['model']['dataType']).'("'.$field->name.'"'.(false===in_array($field->dataType,self::FieldsWithSize)?"":(",".($field->size??self::Defaults['model']['size']))).')'
+                .(isset($field->nullable) && true===$field->nullable?"->nullable(true)":"->nullable(false)")
+                .(isset($field->autoIncrement) && true===$field->autoIncrement?"->autoIncrement()":"")
+                .(isset($field->unsigned) && true===$field->unsigned?"->unsigned()":"")
+                .(false===empty($field->default)?"->default($field->default)":"")
+                .(false===empty($field->charset)?"->charset($field->charset)":"")
+                .(false===empty($field->collation)?"->collation($field->collation)":"")
+                .(isset($field->unique) && true===$field->unique?"->unique()":"")
+                .(isset($field->index) && true===$field->index?"->index()":"")
+                .(isset($field->spatialIndex) && true===$field->spatialIndex?"->spatialIndex()":"")
+                .(isset($field->primary) && true===$field->primary && $field->dataType!='bigIncrements'?"->primary()":"")
+                .';';
+            }
+            if(strlen($fieldList)>0){
+                $contents='
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class '.str_replace('_','',$className).' extends Migration
+{
+    public function up()
+    {
+        if(false===Schema::hasTable("'.$model->modelName.'")){
+            Schema::create("'.$model->modelName.'", function (Blueprint $table) {
+                $table->engine = "'.($model->engine??self::Defaults['model']['engine']).'";
+                $table->charset = "'.($model->charset??self::Defaults['model']['charset_table']).'";
+                $table->collation = "'.($model->collation??self::Defaults['model']['collation']).'";
+                '.$fieldList.'
+            });
+        }
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists("'.$model->modelName.'");
+    }
+}
+?>';
+                $this->RootStorage->put($target,$contents);
+            }else{
+                throw new Exception("Model contains no fields definition [$model->modelName]");
+            }
+        }
     }
 
     public function buildPageTemplate(){
